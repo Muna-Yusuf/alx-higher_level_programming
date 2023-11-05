404f916da2f333c325ad52813f30004b42163909
Muna-Yusuf <muna.y.a.jim@gmail.com>
2023-11-05 08:15:03 -0800

    ...

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     leng = len(sentence)
+    if not sentence:
+        sentence[0] = None
     if leng <= 0:
         return (None)
     return (leng, sentence[0])
