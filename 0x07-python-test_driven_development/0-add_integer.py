e5bf1365a335b0d0be29d1ee165edcf97ce13d8f
Muna-Yusuf <Muna.y.a.jim@gmail.com>
2023-11-25 09:58:49 -0800

    ...

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-"""Module for add_integer mothod"""
 
+"""Module for add_integer mothod
+    it adds 2 integers
+    a and b must be first casted
+
+    """
 
 def add_integer(a, b=98):
     """Function Returns an integer: the addition of a and b
-
-      Args:
-            a: number 1.
-            b: number 2.
-       Raises:
-            TypeError: if a or b are not in (int, float).
-       Return: the sum of a and b.
-
+       Raises: TypeError: if a or b are not in (int, float).
     """
     if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
@@ -19,7 +16,6 @@ def add_integer(a, b=98):
         raise TypeError("b must be an integer")
     return (int(a) + int(b))
 
-
     if __name__ == "__main__":
-    import doctest
-    doctest.testfile("tests/0-add_integer.txt")
+        import doctest
+        doctest.testfile("tests/0-add_integer.txt")
