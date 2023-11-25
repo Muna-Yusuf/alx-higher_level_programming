a4628d6fd534eac4c47b23ba4e0ce59de3406caa
Muna-Yusuf <Muna.y.a.jim@gmail.com>
2023-11-25 06:11:48 -0800

    ...

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,7 +5,7 @@
 def add_integer(a, b=98):
     """Function Returns an integer: the addition of a and b
 
-       Args:
+      Args:
             a: number 1.
             b: number 2.
        Raises:
@@ -13,13 +13,13 @@ def add_integer(a, b=98):
        Return: the sum of a and b.
 
     """
-    if type(a) not in (int, float):
+    if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
-    if type(b) not in (int, float):
+    if ((not isinstance(b, int) and not isinstance(b, float))):
         raise TypeError("b must be an integer")
     return (int(a) + int(b))
 
 
-if __name__ == "__main__":
+    if __name__ == "__main__":
     import doctest
     doctest.testfile("tests/0-add_integer.txt")
--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,2 +1,40 @@
 #!/usr/bin/python3
+"""Module for add_integer mothod"""
 
+
+def matrix_divided(matrix, div):
+    """Function that divides all elements of a matrix.
+
+       Args:
+            matrix: A list of lists of integers or floats.
+            div: The number that will be divided by the list. 
+       Raises:
+            TypeError: If the list are not in (int, float).
+            TypeError: If rows of the matrix are not the same size.
+            TypeError: IF div are not in (int, float).
+            ZeroDivisionError: If div == 0.
+       Return: A new matrix.
+
+    """
+    if (not isinstance(matrix, list) or matrix == [] or
+            not all(isinstance(row, list) for row in matrix) or
+            not all((isinstance(ele, int) or isinstance(ele, float))
+                    for ele in [num for row in matrix for num in row])):
+        raise TypeError("matrix must be a matrix (list of lists) of "
+                        "integers/floats")
+
+    if not all(len(row) == len(matrix[0]) for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+
+    if not isinstance(div, int) and not isinstance(div, float):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
+    
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/2-matrix_divided.txt")
