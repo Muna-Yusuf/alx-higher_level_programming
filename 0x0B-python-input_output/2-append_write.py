02a878bf2acb71a98f04e122b3cd7975f42f6b9d
Muna-Yusuf <Muna.y.a.jim@gmail.com>
2023-12-05 08:19:15 -0800

    ...

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""Defines file-reading function."""
+"""This module defines a file-appending function."""
 
 
-def write_file(filename="", text=""):
-    """ function that appends a string at the end of a text file (UTF8)"""
+def append_write(filename="", text=""):
+    """Appends a string to the end of a UTF8 text file
+    """
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
