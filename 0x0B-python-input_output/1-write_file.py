77411bed7a66ae47800de079541228cc5249d8ba
Muna-Yusuf <Muna.y.a.jim@gmail.com>
2023-12-05 08:12:01 -0800

    ...

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines file-reading function."""
+
+
+def write_file(filename="", text=""):
+    """ reads a text file (UTF8) and prints it to stdout."""
+    with open(filename, "w", encoding='utf-8') as f:
+        return f.write(text)
