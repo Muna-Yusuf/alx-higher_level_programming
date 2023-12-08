15f265321c3311b68cd5c5a071e58b4c6cdc7a9f
Muna-Yusuf <Muna.y.a.jim@gmail.com>
2023-12-08 09:35:41 -0800

    ...

---
+++ 0x0C-python-almost_a_circle/README.md
@@ -0,0 +1,9 @@
+0x0C. Python - Almost a circle
+Python OOP
+
+    By: Guillaume
+    Weight: 1
+    Project will start Dec 7, 2023 6:00 AM, must end by Dec 12, 2023 6:00 AM
+    Checker will be released at Dec 11, 2023 6:00 PM
+    Manual QA review must be done (request it when you are done with the project)
+    An auto review will be launched at the deadline
---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,112 @@
+#!/usr/bin/python3
+"""defines class Rectangle"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """ class Rectangle."""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """ Constructor.
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """ width of the rectangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.validate_integer("width", value, False)
+        self.__width = value
+
+    @property
+    def height(self):
+        """height of the rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.validate_integer("height", value, False)
+        self.__height = value
+
+    @property
+    def x(self):
+        """ x of the rectangle"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.validate_integer("x", value)
+        self.__x = value
+
+    @property
+    def y(self):
+        """ y of the rectangle"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.validate_integer("y", value)
+        self.__y = value
+
+    def validate_integer(self, name, value, eq=True):
+        """validation of all setter methods"""
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if eq and value < 0:
+            raise ValueError("{} must be >= 0".format(name))
+        elif not eq and value <= 0:
+            raise ValueError("{} must be > 0".format(name))
+
+    def area(self):
+        """ the area of the Rectangle."""
+        return self.width * self.height
+
+    def display(self):
+        """ prints the Rectangle instance with the character #"""
+        for _ in range(self.y):
+            print()
+        for _ in range(self.height):
+            print(" " * self.x + "#" * self.width)
+
+    def __str__(self):
+        """Return the print() and str() representation of the Rectangle."""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
+                                                       self.x,
+                                                       self.y,
+                                                       self.width,
+                                                       self.height)
+
+    def update(self, *args, **kwargs):
+        """assigns an argument to each attribute"""
+        if args:
+            for counts, arg in enumerate(args):
+                if counts == 0:
+                    self.id = arg
+                elif counts == 1:
+                    self.width = arg
+                elif counts == 2:
+                    self.height = arg
+                elif counts == 3:
+                    self.x = arg
+                elif counts == 4:
+                    self.y = arg
+
+        elif len(kwargs) > 0:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                elif key == "width":
+                    self.width = value
+                elif key == "height":
+                    self.height = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
