a7c386521e530a13ca42aeb3ce6cbd5241bc849f
Muna-Yusuf <muna.y.a.jim@gmail.com>
2024-01-17 10:22:25 -0800

    ..

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,12 @@
+-- Import the database dump of hbtn_0d_tvshows.
+-- script that lists all shows contained in the database hbtn_0d_tvshows.
+SELECT name
+FROM tv_genres
+WHERE name NOT IN
+(SELECT name
+FROM tv_genres
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = 'Dexter')
+GROUP BY name
+ORDER BY name ASC;
