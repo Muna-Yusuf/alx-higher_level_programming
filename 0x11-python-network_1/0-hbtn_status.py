4a9b02ccb21d7304b00b54af5acd275bf1176381
Muna-Yusuf <muna.y.a.jim@gmail.com>
2024-03-01 12:38:27 -0800

    0. What's my status?

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
         resp_type = type(resp_body)
         resp_deco = resp_body.decode("utf-8")
         print("Body response:")
-        print(f"\t- type:{resp_type}")
+        print(f"\t- type: {resp_type}")
         print(f"\t- content: {resp_body}")
         print(f"\t- utf8 content: {resp_deco}")
